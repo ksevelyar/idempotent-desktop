4501fc9dd9edcc7c6cf752a29ce28f0cebf4f0d3
Sergey Zubkov <ksevelyar@gmail.com>
2020-04-24 01:10:19 +0300

    simplify postgres auth

--- modules/dev-packages.nix
+++ modules/dev-packages.nix
@@ -3,12 +3,7 @@
 {
   services.postgresql = {
     enable = true;
-    authentication = ''
-      # TYPE  DATABASE        USER            ADDRESS                 METHOD
-      local   all             all                                     trust
-      host    all             all             127.0.0.1/32            trust
-      host    all             all             ::1/128                 trust
-    '';
+    authentication = "local all all trust";
   };
 
   environment.systemPackages = with pkgs;
