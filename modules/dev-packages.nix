3a2b9e8fb0e98f002e13c7de1f93bb9f7a75bd23
Sergey Zubkov <ksevelyar@gmail.com>
2020-04-24 01:03:01 +0300

    add postgres

--- modules/dev-packages.nix
+++ modules/dev-packages.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  services.postgresql = {
+    enable = true;
+    authentication = ''
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            trust
+      host    all             all             ::1/128                 trust
+    '';
+  };
+
   environment.systemPackages = with pkgs;
     [
       # images
--- modules/extra-packages.nix
+++ modules/extra-packages.nix
@@ -19,6 +19,7 @@ in
       # boot
 
       # cli
+      stable.khal
       calcurse
       wtf
       asciiquarium
