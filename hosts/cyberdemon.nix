ea3c82e181928518faf5b60e7018b40067f70a92
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-04 14:13:39 +0300

    bump cyberdemon config

--- hosts/cyberdemon.nix
+++ hosts/cyberdemon.nix
@@ -20,7 +20,8 @@
       ../modules/sound.nix
       ../modules/firewall-desktop.nix
       ../modules/fonts.nix
-      ../modules/nebula.nix
+      # ../modules/nebula.nix
+      ../modules/wireguard.nix
       ../modules/ssd.nix
 
       ../modules/vm/hypervisor.nix
@@ -29,10 +30,43 @@
       ../users/manya.nix
     ];
 
-  environment.etc."/nebula/node.crt".source = /storage/nebula/cyberdemon.crt;
-  environment.etc."/nebula/node.key".source = /storage/nebula/cyberdemon.key;
-  environment.etc."/nebula/node.yml".source = /storage/nebula/node.yml;
-  environment.etc."/nebula/ca.crt".source = /storage/nebula/ca.crt;
+  # environment.etc."/nebula/node.crt".source = /storage/nebula/cyberdemon.crt;
+  # environment.etc."/nebula/node.key".source = /storage/nebula/cyberdemon.key;
+  # environment.etc."/nebula/node.yml".source = /storage/nebula/node.yml;
+  # environment.etc."/nebula/ca.crt".source = /storage/nebula/ca.crt;
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    skynet = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      ips = [ "192.168.42.2/24" ];
+
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/home/manya/wireguard-keys/private";
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+        {
+          # Public key of the server (not a file path).
+          publicKey = "YruKx4tFhi+LfPgkhSp4IeHZD0lszSMxANGvzyJW4jY=";
+
+          # Forward all the traffic via VPN.
+          allowedIPs = [ "0.0.0.0/0" ];
+          # Or forward only particular subnets
+          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+
+          # Set this to the server IP and port.
+          endpoint = "{77.37.166.17:51820}";
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [];
