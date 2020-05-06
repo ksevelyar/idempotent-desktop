91d8c52f42e408b2e82568cd88326b304232c1ff
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-07 00:32:40 +0300

    add nginx with auto certs

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -1,15 +1,28 @@
 {
+  security.acme.acceptTerms = true;
+
   services.nginx = {
+    # Use recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    commonHttpConfig = ''
+      charset utf-8;
+      source_charset utf-8;
+    '';
+
     enable = true;
     virtualHosts."dobroserver.ru" = {
       enableACME = true;
       forceSSL = true;
-      root = "/home/ksevelyar/dobroserver";
+      root = "/var/www/dobroserver";
     };
   };
   # Optional: You can configure the email address used with Let's Encrypt.
   # This way you get renewal reminders (automated by NixOS) as well as expiration emails.
   security.acme.certs = {
-    "blog.example.com".email = "ksevelyar@gmail.com";
+    "dobroserver.ru".email = "ksevelyar@gmail.com";
   };
 }
