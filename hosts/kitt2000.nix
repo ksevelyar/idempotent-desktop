956a5be1a9a5362f23aa214a34d44622f3446a62
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-09 16:06:27 +0300

    add hosts/kitt2000

--- hosts/kitt2000.nix
+++ hosts/kitt2000.nix
@@ -1,17 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  boot.cleanTmpDir = lib.mkDefault true;
-  boot.tmpOnTmpfs = lib.mkDefault true;
-
-  nix = {
-    useSandbox = true;
-    maxJobs = lib.mkDefault 2;
-    extraOptions = ''
-      connect-timeout = 10 
-      http-connections = 4
-    '';
-  };
-
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
@@ -21,44 +9,46 @@
   imports =
     [
       <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-      ../modules/aliases.nix
-      # FIXME: find replacement for memtest
-      ../modules/absolutely-proprietary.nix
-      ../modules/scripts.nix
+      ../modules/sys/aliases.nix
+      ../modules/sys/scripts.nix
+      # ../modules/sys/debug.nix
+
       ../modules/boot/bios.nix
+      # ../modules/boot/multiboot.nix
 
-      ../modules/dev/nvim.nix
-      ../modules/dev/packages.nix
+      ../modules/services/common.nix
+      ../modules/services/nginx.nix
+      # ../modules/services/nfs.nix
 
-      ../modules/common-packages.nix
-      ../modules/ssd.nix
-      ../modules/router.nix
-      # ./modules/extra-packages.nix
-      # ./modules/dev-packages.nix
-      # ./modules/games.nix
-      ../modules/services-headless.nix
-      # ./modules/x.nix
+      ../modules/packages/absolutely-proprietary.nix
+      ../modules/packages/common.nix
+      ../modules/packages/nvim.nix
+      ../modules/packages/tmux.nix
 
-      # ./modules/fonts.nix
-      # ./modules/fonts-high-dpi.nix
-      # ./modules/laptop.nix
+      ../modules/hardware/ssd.nix
+      ../modules/hardware/bluetooth.nix
+
+      ../modules/net/wireguard.nix
 
       ../users/ksevelyar-headless.nix
     ];
 
+  boot.cleanTmpDir = true;
+  boot.tmpOnTmpfs = true;
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ata_piix" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
 
+  networking.hostName = "kitt2000";
   networking.firewall.enable = lib.mkForce true;
-  networking.networkmanager.enable = lib.mkDefault true; # run nmtui for wi-fi
+  networking.networkmanager.enable = true; # run nmtui for wi-fi
   networking.useDHCP = false;
   networking.interfaces.wlp3s0.useDHCP = true;
   networking.useDHCP = false;
   networking.interfaces.eno0.useDHCP = true;
-  networking.hostName = "satellite";
 
+  # sudo e2label /dev/disk/by-uuid/44b4a02e-1993-4470-b345-b2ca5e3e5b42 nixos
   fileSystems."/" =
     {
       device = "/dev/disk/by-uuid/4aaad1a6-008e-422b-aa45-0c2cbb0e8f27";
