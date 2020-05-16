15161eea7e1f100d79deb5af5f0d24efffea94fa
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-16 23:22:40 +0300

    bump murmur

--- modules/services/murmur.nix
+++ modules/services/murmur.nix
@@ -2,6 +2,10 @@
   services.murmur = {
     enable = true;
     welcometext = "🥂";
+    users = 50;
+    registerName = "skynet";
+    clientCertRequired = true;
+    bandwhich = 100000;
   };
 
   networking.firewall.allowedTCPPorts = [ 64738 ];
