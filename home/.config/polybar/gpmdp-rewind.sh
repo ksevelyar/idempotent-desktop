2fa638ef50116b8198f9cb0acffe90953a0903fc
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-27 02:03:04 +0300

    Delete gpmdp-rewind.sh

--- home/.config/polybar/gpmdp-rewind.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-COMMANDS='
-{"namespace": "connect","method": "connect","arguments": ["cli", "621b2119-3de8-4d66-827c-588bd0c603fd"]}
-{"namespace": "playback","method": "forward"}
-'
-
-printf "%s\n" '{"namespace": "connect","method": "connect","arguments": ["cli", "621b2119-3de8-4d66-827c-588bd0c603fd"]}' '{"namespace": "playback","method": "rewind"}' | websocat ws://127.0.0.1:5672
