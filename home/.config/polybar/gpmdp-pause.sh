7906d05543a849e80ba6e513b274b24d1a4c5c81
Sergey Zubkov <ksevelyar@gmail.com>
2020-05-27 02:02:57 +0300

    Delete gpmdp-pause.sh

--- home/.config/polybar/gpmdp-pause.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-printf "%s\n" \
-  '{"namespace": "connect","method": "connect","arguments": ["cli", "621b2119-3de8-4d66-827c-588bd0c603fd"]}' \
-  '{"namespace": "playback","method": "playPause"}' | websocat ws://127.0.0.1:5672
