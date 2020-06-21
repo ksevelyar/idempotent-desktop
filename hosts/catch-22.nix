c9c534a2d09e181699aaedafad641913899b7a9f
Tatiana Kh <ts.khol@gmail.com>
2020-06-21 17:23:37 +0300

    add hosts/catch-22.nix

---
+++ hosts/catch-22.nix
@@ -0,0 +1,127 @@
+{ config, lib, pkgs, ... }:
+{
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+
+  imports =
+    [
+      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+
+      ../users/shared.nix
+      ../users/kh.nix
+
+      ../modules/sys/aliases.nix
+      # ../modules/sys/debug.nix
+      ../modules/sys/nix.nix
+      ../modules/sys/scripts.nix
+      ../modules/sys/sysctl.nix
+      ../modules/sys/tty.nix
+      ../modules/sys/vars.nix
+
+      ../modules/boot/efi.nix
+      ../modules/boot/multiboot.nix
+
+      ../modules/services/common.nix
+      ../modules/services/x.nix
+
+      ../modules/x/xmonad.nix
+      ../modules/x/fonts.nix
+      ../modules/packages/x-common.nix
+      ../modules/packages/x-extra.nix
+
+      ../modules/packages/absolutely-proprietary.nix
+      ../modules/packages/common.nix
+      ../modules/packages/dev.nix
+      ../modules/packages/games.nix
+      ../modules/packages/nvim.nix
+      ../modules/packages/pass.nix
+      ../modules/packages/tmux.nix
+      ../modules/packages/firefox.nix
+
+      ../modules/hardware/bluetooth.nix
+      ../modules/hardware/sound.nix
+      ../modules/hardware/ssd.nix
+      # ../modules/hardware/laptop.nix
+
+      ../modules/net/firewall-desktop.nix
+      ../modules/net/wireguard.nix
+      ../modules/net/i2pd.nix
+      ../modules/net/tor.nix
+      ../modules/net/sshd.nix
+      ../modules/net/openvpn.nix
+      # ../modules/net/lidarr.nix
+
+      ../modules/vm/hypervisor.nix
+    ];
+
+  boot.loader.grub.splashImage = lib.mkForce ../assets/grub_big.png;
+  boot.loader.grub.backgroundColor = lib.mkForce "#09090B";
+
+  networking.hostName = "catch-22";
+  networking.firewall.enable = lib.mkForce true;
+  networking.networkmanager.enable = true; # run nmtui for wi-fi
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.enp4s0.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # mkdir -p ~/wireguard-keys && cd ~/wireguard-keys && umask 077
+  # wg genkey | tee private | wg pubkey > public
+  networking.wireguard.interfaces = {
+    skynet = {
+      ips = [ "192.168.42.10" ];
+      privateKeyFile = "/home/kh/wireguard-keys/private";
+
+      peers = [
+        {
+          publicKey = "YruKx4tFhi+LfPgkhSp4IeHZD0lszSMxANGvzyJW4jY=";
+          allowedIPs = [ "192.168.42.0/24" ];
+          endpoint = "77.37.166.17:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+  fileSystems."/skynet" = {
+    device = "192.168.42.1:/export";
+    fsType = "nfs";
+  };
+
+  boot.cleanTmpDir = true;
+  boot.tmpOnTmpfs = true;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/21520a28-cf26-42a0-aaf6-17f8e6e62f36";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/88D8-38E9";
+      fsType = "vfat";
+    };
+
+  swapDevices = [];
+
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # console.font = lib.mkForce "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  # services.xserver.dpi = 180;
+  # environment.variables = {
+  #   GDK_SCALE = "2";
+  #   GDK_DPI_SCALE = "0.5";
+  #   _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  # };
+}
