530cdf0fb6980afa004de84fd8cbae789189d95b
Sergey Zubkov <ksevelyar@gmail.com>
2020-06-25 23:05:02 +0300

    fix users/obstinatekate.nix

--- users/obstinatekate.nix
+++ users/obstinatekate.nix
@@ -8,7 +8,6 @@
         enable = true;
         userName = "Ekaterina Lobanova";
         userEmail = "obstinatekate@gmail.com";
-        aliases.lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       };
     };
   };
