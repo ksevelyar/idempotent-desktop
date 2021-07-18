c3f6dde6ff1cf32f767c93557c86d466604e12d3
Sergey Zubkov <ksevelyar@gmail.com>
2021-07-19 01:01:33 +0300

    fix conky glitch

--- users/shared/.config/conky/launch.sh
+++ users/shared/.config/conky/launch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pkill -9 conky
 
-conky -c ~/.config/conky/conky-taskwarrior.conf -d
-conky -c ~/.config/conky/conky-lyrics.conf -d
+conky -c ~/.config/conky/conky-taskwarrior.conf --daemonize --pause=3
+conky -c ~/.config/conky/conky-lyrics.conf --daemonize --pause=3 
