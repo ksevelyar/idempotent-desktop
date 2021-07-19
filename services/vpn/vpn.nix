7b67a4c7d631ab694575e7cae66e3d43f0ed82a4
Sergey Zubkov <ksevelyar@gmail.com>
2021-07-19 14:47:45 +0300

    bump vpn.nix

--- services/vpn/vpn.nix
+++ services/vpn/vpn.nix
@@ -18,6 +18,8 @@
       commands = [
         { command = "/run/current-system/sw/bin/systemctl start openvpn-uk-shark.service"; options = [ "SETENV" "NOPASSWD" ]; }
         { command = "/run/current-system/sw/bin/systemctl stop openvpn-uk-shark.service"; options = [ "SETENV" "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/systemctl restart openvpn-uk-shark.service"; options = [ "SETENV" "NOPASSWD" ]; }
+        { command = "/run/current-system/sw/bin/systemctl status openvpn-uk-shark.service"; options = [ "SETENV" "NOPASSWD" ]; }
       ];
     }
   ];
