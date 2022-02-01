37f26920b318614f9bf50e42681315ab0e73a0d1
Sergey Zubkov <ksevelyar@gmail.com>
2022-02-01 20:27:54 +0300

    bump binary cache syntax, nix flake update

--- sys/nix.nix
+++ sys/nix.nix
@@ -2,14 +2,15 @@
 {
   nix = {
     package = pkgs.nixFlakes;
-    useSandbox = true;
-
-    binaryCaches = [
-      "https://idempotent-desktop.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "idempotent-desktop.cachix.org-1:OkWDud90b2/k/k1yIUg1lxZdNRWEvCfv6zSSRQ75lVM="
-    ];
+    settings = {
+      sandbox = true;
+      substituters = [
+        "https://idempotent-desktop.cachix.org"
+      ];
+      trusted-public-keys = [
+        "idempotent-desktop.cachix.org-1:OkWDud90b2/k/k1yIUg1lxZdNRWEvCfv6zSSRQ75lVM="
+      ];
+    };
 
     # 🍑 smooth rebuilds
     daemonCPUSchedPolicy = "idle";
