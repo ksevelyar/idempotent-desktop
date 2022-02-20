648beaf3918c42b4f65c796013746ec24dc0dffa
Sergey Zubkov <ksevelyar@gmail.com>
2022-02-21 01:27:15 +0300

    clean up

--- packages/electronics.nix
+++ packages/electronics.nix
@@ -6,26 +6,13 @@
     [
       fritzing
       librepcb
-      xoscope
-
-      # platformio project init --ide vim --board bluepill_f103c8
-      platformio
-
-      # arduino
-      # arduino --board arduino:avr:nano --port /dev/ttyUSB0 --upload shroom-box.ino
-      # arduino
-      # arduino-core
-
-      # esp8266 / 32
-      # esptool
-      # nix-shell https://github.com/nix-community/nix-environments/archive/master.tar.gz -A arduino
+      # xoscope
 
       # stm32
-      stlink
-      openocd
-      gcc-arm-embedded
+      # stlink
+      # openocd
+      # gcc-arm-embedded
       # stm32cubemx
-      gnumake
-      # python
+      # gnumake
     ];
 }
