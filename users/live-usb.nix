299dbeb9f31cec116d27da38d309f68cfa2cc96f
Sergey Zubkov <ksevelyar@gmail.com>
2022-03-03 20:02:54 +0300

    leftwm for ksevelyar & live usb

--- users/live-usb.nix
+++ users/live-usb.nix
@@ -9,7 +9,7 @@ in
   imports = [
     (import ./shared.nix (args // { user = user; email = email; name = name; }))
   ] ++ [
-    (lib.mkIf (config.services.xserver.enable) (import ../services/x/xmonad.nix (args // { user = user; })))
+    (lib.mkIf (config.services.xserver.enable) (import ../services/x/leftwm.nix (args // { user = user; })))
     (lib.mkIf (config.services.xserver.enable) (import ../services/x/polybar.nix (args // { user = user; })))
     (lib.mkIf (config.services.xserver.enable) (import ../packages/firefox.nix (args // { user = user; })))
   ];
