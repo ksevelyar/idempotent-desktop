665a1c71b8a111c3b5877ee9ff495bd2969195d7
Sergey Zubkov <ksevelyar@protonmail.com>
2022-04-24 00:48:03 +0300

    remove legacy firefox files

--- packages/firefox.nix
+++ packages/firefox.nix
@@ -18,10 +18,8 @@
 
             "browser.uidensity" = 1; # minimal ui
             "permissions.default.desktop-notification" = 2; # disable all notifications requests
-            "ui.context_menus.after_mouseup" = true; # fix right mouse click in xmonad
+            "ui.context_menus.after_mouseup" = true; # fix right mouse click in tiling wm
             "svg.context-properties.content.enabled" = true; # allow dark theme for simple tab groups
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # allow to use userChrome.css
-            "layers.acceleration.force-enabled" = true; # fix wrong geometry in xmonad after restart
             "signon.rememberSignons" = false; # disable built-in password manager
             "browser.startup.page" = 3; # restore last session
             "browser.urlbar.suggest.history" = false;
