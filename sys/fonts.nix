7e673d787c9da5b43b4f0b53b6d116808c47ceaf
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-09 22:41:11 +0300

    add source-code-pro

--- sys/fonts.nix
+++ sys/fonts.nix
@@ -3,14 +3,14 @@
 {
   fonts = {
     enableDefaultFonts = true;
-    fontDir.enable = true; # ls /run/current-system/sw/share/X11-fonts
+    fontDir.enable = true; # ls /run/current-system/sw/share/X11/fonts/
     fontconfig = {
       enable = true;
       cache32Bit = true;
       hinting.enable = true;
       antialias = true;
       defaultFonts = {
-        monospace = [ "Terminus" ];
+        monospace = [ "Source Code Pro" ];
         sansSerif = [ "Roboto" ];
         serif = [ "Roboto Slab" ];
       };
@@ -19,22 +19,17 @@
     fonts = with pkgs;
       [
         terminus_font
-        unifont
-        cozette
-        fixedsys-excelsior
-        noto-fonts-emoji
-
-        # unfree Microsoft fonts
-        corefonts # Andale Mono, Arial, Comic Sans, Courier New, Georgia, Impact, Times New Roman, Trebuchet, Verdana, Webdings
-
-        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerdfonts/shas.nix
-        (nerdfonts.override { fonts = [ "Iosevka" "IBMPlexMono" ]; })
-        fira-code
-
         # Icons for polybar
         siji # https://github.com/stark/siji
+        cozette
 
-        open-sans
+        noto-fonts-emoji
+
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerdfonts/shas.nix
+        # (nerdfonts.override { fonts = [ "Iosevka" "IBMPlexMono" ]; })
+        source-sans-pro
+        source-code-pro
+
         roboto
         roboto-slab
         roboto-mono
