31e739f3568571d8acd2414812b695cf52173bd6
Sergey Zubkov <ksevelyar@protonmail.com>
2023-04-15 18:11:15 +0300

    add bartib, rewrite timer function to accept label name

--- packages/common.nix
+++ packages/common.nix
@@ -46,6 +46,9 @@
       smartmontools
       usbutils # lsusb
 
+      # gtd
+      bartib
+
       # sec
       cryptsetup
       pwgen
--- users/shared/fish/config.fish
+++ users/shared/fish/config.fish
@@ -7,6 +7,8 @@ set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_ALT_C_COMMAND 'fd --type d .'
 
+set -gx BARTIB_FILE ~/.activities.bartib
+
 zoxide init fish | source
 
 function ssht # ssht skynet.local
@@ -17,8 +19,11 @@ function pgen # pgen learn/edx.org
     gopass generate -s -c $argv[1] 42 # if you can type your password it's no good
 end
 
-function t # t 12m
-    fish -c "sleep $argv[1] && notify-send -u critical -t 0 ▓▓▓" &
+function t # t 12m or t 9m pizza
+    set label $argv[2]
+    test -z "$label"; and set label "▓▓▓"
+
+    fish -c "sleep $argv[1] && notify-send -u critical -t 0 $label && paplay /etc/nixos/assets/audio/timer.wav" &
 end
 
 function find-and-replace
