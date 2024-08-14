db37585e6ae157a889f3cd52e9c3fc18a974a942
Sergey Zubkov <ksevelyar@protonmail.com>
2024-08-14 13:37:28 +0300

    fix live usb

--- hardware/pipewire.nix
+++ hardware/pipewire.nix
@@ -28,7 +28,7 @@ in {
     pkgs.pulseaudio # pactl
   ];
 
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = lib.mkForce false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
