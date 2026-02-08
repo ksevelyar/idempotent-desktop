4a23e406d2ff1698da365bf28b8ca8ec43f61992
Sergey Zubkov <ksevelyar@protonmail.com>
2026-02-09 00:29:38 +0300

    update ci

--- sys/cache.nix
+++ sys/cache.nix
@@ -9,7 +9,7 @@
         "https://idempotent-desktop.cachix.org"
       ];
       trusted-public-keys = [
-        "idempotent-desktop.cachix.org-1:OkWDud90b2/k/k1yIUg1lxZdNRWEvCfv6zSSRQ75lVM="
+        "idempotent-desktop.cachix.org-1:21i2Mb/mrJ9XcfmksWpaYMr78ZPbwxhX/BwSS1X+PRw="
       ];
     };
   };
