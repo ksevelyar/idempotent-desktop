3b713719462aa5a0a0f176002ed0b41fcddea944
Sergey Zubkov <ksevelyar@protonmail.com>
2026-03-05 11:18:11 +0300

    remove tlp

--- hardware/power-management.nix
+++ hardware/power-management.nix
@@ -3,27 +3,19 @@
   battery,
   ...
 }: {
-  powerManagement.enable = true;
-
-  services.tlp = {
+  powerManagement = {
     enable = true;
-    settings = {
-      # Disable too aggressive power-management autosuspend for USB receiver for wireless mouse
-      USB_AUTOSUSPEND = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    };
+    powertop.enable = true;
+    cpuFreqGovernor = "powersave";
   };
 
   environment.systemPackages = with pkgs; [
-    powertop
     acpi
-    tlp
   ];
 
   systemd.user.timers.notify-on-low-battery = {
-    timerConfig.OnBootSec = "2m";
-    timerConfig.OnUnitInactiveSec = "2m";
+    timerConfig.OnBootSec = "5m";
+    timerConfig.OnUnitInactiveSec = "5m";
     timerConfig.Unit = "notify-on-low-battery.service";
     wantedBy = ["timers.target"];
   };
