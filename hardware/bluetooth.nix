f1e267baab4cb3c8aa5be006216bb1a3370d42ea
Sergey Zubkov <ksevelyar@protonmail.com>
2026-03-06 22:38:28 +0300

    fix blueman for wayland

--- hardware/bluetooth.nix
+++ hardware/bluetooth.nix
@@ -1,13 +1,5 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
-  hardware = {
-    bluetooth.enable = true;
-  };
+{...}: {
+  hardware.bluetooth.enable = true;
 
-  services.blueman.enable = lib.mkIf (config.services.xserver.enable) true;
-  programs.dconf.enable = lib.mkIf (config.services.xserver.enable) true;
+  services.blueman.enable = true;
 }
