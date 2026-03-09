9675e4893a65365de4410652f4a5462cf93982bb
Sergey Zubkov <ksevelyar@protonmail.com>
2026-03-09 20:42:51 +0300

    set monitors for speed-demon

--- hosts/speed-demon.nix
+++ hosts/speed-demon.nix
@@ -56,7 +56,7 @@ args @ {
 
   home-manager.users.kavarkon = {
     home.file.".config/hypr/hypridle.conf".source = ../users/ksevelyar/hk47/hypr/hypridle.conf;
-    home.file.".config/hypr/hyprland.conf".source = ../users/ksevelyar/hk47/hypr/hyprland.conf;
+    home.file.".config/hypr/hyprland.conf".source = ../users/kavarkon/speed-demon/hypr/hyprland.conf;
     home.file.".config/waybar/config".source = ../users/ksevelyar/hk47/waybar/waybar.json;
     home.file.".config/waybar/style.css".source = ../users/ksevelyar/hk47/waybar/waybar.css;
   };
