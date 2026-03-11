441650bfc4a44479e400406ac4f78f70f0fbda64
Sergey Zubkov <ksevelyar@protonmail.com>
2026-03-11 20:25:55 +0300

    simplify spotify setup

--- packages/absolutely-proprietary.nix
+++ packages/absolutely-proprietary.nix
@@ -1,5 +1,8 @@
 {
   nixpkgs.config.allowUnfree = true;
+  # NOTE: enable spotify & netflix
+  nixpkgs.config.chromium.enableWideVine = true;
+
   hardware.enableRedistributableFirmware = true;
   hardware.wirelessRegulatoryDatabase = true;
 }
--- packages/spotify.nix
+++ packages/spotify.nix
@@ -1,11 +1,15 @@
 {pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; [
-      spotify
       playerctl
+
+      (pkgs.writeScriptBin "spotify" ''
+        #!/usr/bin/env bash
+        exec ${pkgs.ungoogled-chromium}/bin/chromium \
+          --proxy-server=socks5://127.0.0.1:2081 \
+          --app="https://open.spotify.com" \
+          --class="spotify-scratchpad"
+      '')
     ];
   };
-
-  networking.firewall.allowedTCPPorts = [57621];
-  networking.firewall.allowedUDPPorts = [5353];
 }
