e3d16d2bef0df4cf1b22149866965d1ad11dffe7
Sergey Zubkov <ksevelyar@protonmail.com>
2026-03-11 11:57:40 +0300

    remove redundant kernel options

--- hardware/amd-cpu.nix
+++ hardware/amd-cpu.nix
@@ -1,8 +1,3 @@
-{config, ...}: {
+{
   hardware.cpu.amd.updateMicrocode = true;
-
-  boot.kernelModules = ["kvm-amd"];
-  boot.initrd.availableKernelModules = [
-    "cryptd"
-  ];
 }
--- hardware/intel-cpu.nix
+++ hardware/intel-cpu.nix
@@ -1,10 +1,4 @@
-{config, ...}: {
-  boot.kernelModules = ["kvm-intel"];
+{
   boot.kernelParams = ["nosgx"];
   hardware.cpu.intel.updateMicrocode = true;
-
-  boot.initrd.availableKernelModules = [
-    "aesni_intel"
-    "cryptd"
-  ];
 }
